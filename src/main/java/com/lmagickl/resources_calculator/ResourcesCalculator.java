9300c4f9b2810e6b088dfd2741b3282b00bfbecf
Denis Karupovic <d.karupovic@hotmail.com>
2022-05-03 13:13:34 +0200

    initial commit

--- src/main/java/com/lmagickl/resources_calculator/ResourcesCalculator.java
+++ src/main/java/com/lmagickl/resources_calculator/ResourcesCalculator.java
@@ -1,15 +1,15 @@
-package com.example.examplemod;
+package com.lmagickl.resources_calculator;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = ResourcesCalculator.MODID, version = ResourcesCalculator.VERSION)
+public class ResourcesCalculator
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "resources-calculator";
+    public static final String VERSION = "0.1.0";
 
     @EventHandler
     public void init(FMLInitializationEvent event)
